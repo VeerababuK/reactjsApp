bd650b69ff40e5d739342b5c38241b6af6bdf802
Veerababu Kanumilli <kanumilli.veerababu@gmail.com>
2017-05-15 00:58:13 +0530

    Chapter 5 completed

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -4,12 +4,19 @@ import Footer from "./Footer";
 import Header from "./Header";
 
 export default class Layout extends React.Component{
+	constructor() {
+		super();
+		this.state = {title: 'Welcome'}
+	}
+
+	changeTitle(title) {
+		this.setState({title})
+	}
+	
 	render() {
-const title = 'Welcome Veera!!!';
 		return (
-			<div>			
-				<Header name={'arjuna'} title={title}/>
-				<Header name={'Other'} title={"Other Title"}/>
+			<div>
+				<Header changeTitle={this.changeTitle.bind(this)} title={this.state.title}/>
 				<Footer />
 			</div>
 		);
